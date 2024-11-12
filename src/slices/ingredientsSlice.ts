79392d3192cfbdd8346e7f68b789dddfead6e4ee
Ivan Shurpin <ivanshurpin@yandex.ru>
2024-11-12 23:49:52 +0300

    ingredients

--- src/slices/ingredientsSlice.ts
+++ src/slices/ingredientsSlice.ts
@@ -4,11 +4,17 @@ import { TIngredient } from '@utils-types';
 
 interface IngredientsListState {
   data: TIngredient[];
+  buns: TIngredient[];
+  mains: TIngredient[];
+  sauces: TIngredient[];
   isLoading: boolean;
 }
 
 const initialState: IngredientsListState = {
   data: [],
+  buns: [],
+  mains: [],
+  sauces: [],
   isLoading: true
 };
 
@@ -39,20 +45,45 @@ const ingredientsSlice = createSlice({
     builder.addCase(
       fetchTracks.fulfilled,
       (state: IngredientsListState, action: PayloadAction<TIngredient[]>) => {
+        const buns: TIngredient[] = [];
+        const mains: TIngredient[] = [];
+        const sauces: TIngredient[] = [];
+        action.payload.forEach((item) => {
+          if (item.type === 'main') {
+            mains.push(item);
+          }
+          if (item.type === 'sauce') {
+            sauces.push(item);
+          }
+          if (item.type === 'bun') {
+            buns.push(item);
+          }
+        });
         // Add user to the state array
         state.data = action.payload;
+        state.buns = buns; // action.payload;
+        state.mains = mains; // action.payload;
+        state.sauces = sauces; // action.payload;
         state.isLoading = false;
       }
     );
   },
   selectors: {
     selectIngredients: (sliceState) => sliceState.data,
+    selectIngredientsBuns: (sliceState) => sliceState.buns,
+    selectIngredientsMains: (sliceState) => sliceState.mains,
+    selectIngredientsSauces: (sliceState) => sliceState.sauces,
     selectIsLoadingIngredients: (sliceState) => sliceState.isLoading
   }
 });
 
-export const { selectIngredients, selectIsLoadingIngredients } =
-  ingredientsSlice.selectors;
+export const {
+  selectIngredients,
+  selectIsLoadingIngredients,
+  selectIngredientsBuns,
+  selectIngredientsMains,
+  selectIngredientsSauces
+} = ingredientsSlice.selectors;
 // export const { toggleLike } = ingredientsSlice.actions;
 
 export default ingredientsSlice.reducer;
